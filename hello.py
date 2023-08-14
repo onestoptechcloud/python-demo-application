b9ae848450ad5f559e48b0edd6f120e9e2d6350a
Ubuntu <azureuser@migration-1-vm.jyr2v5oljige1ntdemlf4tdske.phxx.internal.cloudapp.net>
2023-08-14 03:43:08 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+@app.route("/demo")
+def demo_world():
+    return "<p>demo, demo</p>"
